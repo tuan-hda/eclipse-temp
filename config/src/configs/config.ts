02399fe69631d3ce79d509c929104d9eeeaf822b
Tuan Hoang Dinh Anh <tuan.hoangdinhanh@vn.bosch.com>
2024-09-13 10:44:31 +0000

    update with xhub styles

--- config/src/configs/config.ts
+++ config/src/configs/config.ts
@@ -1,5 +1,5 @@
 const config: any = {
-  serverBaseUrl: "https://backend-core-dev.digital.auto",
+  serverBaseUrl: "https://backend-core-xhub.digital.auto",
   serverVersion: import.meta.env.VITE_SERVER_VERSION || "v2",
   logBaseUrl: import.meta.env.PROD
     ? 'https://logs.digital.auto'
@@ -11,33 +11,43 @@ const config: any = {
   widgetMarketPlaceUrl: "https://marketplace.digital.auto/packagetype/widget",
   widgetMarketPlaceBe: "https://store-be.digitalauto.tech",
   uploadFileUrl: "https://upload.digitalauto.tech",
-  instanceLogo:
-    "https://covesa.global/wp-content/uploads/2024/03/covesa_logo.png",
+  instanceLogo: "https://www.bosch.com/media/tech/tech_images/favicon-png.png",
+  instance: "xhub",
   sso: "bosch",
-  instance: "digitalauto",
   defaultModelId: "665826e3194aff003dd2f67b",
   genAI: {
-    defaultEndpointUrl: "https://backend-core-dev.digital.auto/v2/genai",
+    defaultEndpointUrl: "https://backend-core-xhub.digital.auto/v2/genai",
     marketplaceUrl: "https://store-be.digitalauto.tech/marketplace/genai",
     sdvApp: [
-    {
-        id: 'mock-genai',
-        type: 'GenAI_Python',
-        name: 'Mock SDV GenAI',
-        description: 'Mock GenAI for Python code generation',
-        apiKey: 'Empty',
-        endpointUrl: 'https://backend-core-etas.digital.auto/v2/genai/etas',
+      {
+        id: "etas-sdv-genai",
+        type: "GenAI_Python",
+        name: "ETAS SDV GenAI",
+        description: "ETAS GenAI for Python code generation",
+        apiKey: "Empty",
+        endpointUrl: "https://backend-core-xhub.digital.auto/v2/genai",
         customPayload: (prompt: string) => ({ prompt }),
-        isMock: true,
       },
-   ],
-    dashboard: [],
+    ],
+    dashboard: [
+      {
+        id: "etas-dashboard-genai",
+        type: "GenAI_Python",
+        name: "ETAS Dashboard GenAI",
+        description: "ETAS GenAI for Python code generation",
+        apiKey: "Empty",
+        endpointUrl: "https://backend-core-xhub.digital.auto/v2/genai",
+        customPayload: (prompt: string) => ({ prompt }),
+      },
+    ],
     widget: [],
   },
   github: {
     clientId: 'Ov23livVYo2MXyqoIHox'
   },
   disableEmailLogin: import.meta.env.VITE_DISABLE_EMAIL_LOGIN === 'true',
+  strictAuth: true,
 };
 
 export default config;
+	
